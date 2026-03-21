35341ed263011222fd6dccc6d7aa2f210d02aefe
masecho <yenygorden@gmail.com>
2026-03-21 13:04:43 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'yeny-gorden-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
