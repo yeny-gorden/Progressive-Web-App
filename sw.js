cfcbda845a716d51c41d05c0b154a0b6a7c0fee0
masecho <yenygorden@gmail.com>
2026-03-21 13:28:09 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,35 @@
-// Service Worker Minimal agar PWA bisa diinstal
-self.addEventListener('fetch', function(event) {
-    // Biarkan aplikasi mengambil data dari internet secara normal
-    event.respondWith(fetch(event.request));
+const CACHE_NAME = 'yeny-gorden-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html' // Pastikan nama file HTML Anda sesuai, atau biarkan './' saja
+];
+
+// Tahap Install: Menyimpan file ke dalam cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Tahap Fetch: Mengambil data dari cache jika offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Tahap Aktivasi: Menghapus cache lama jika ada update
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
 });
