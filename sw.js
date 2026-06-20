5fd2456727e564d72483b85ba00b7f706eedadb4
masecho <yenygorden@gmail.com>
2026-06-20 15:00:27 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,8 @@
-const CACHE_NAME = 'Kalkulator Gorden Pro v4.1 - Yeny Gorden';
-const ASSETS = [
-  './',
-  './index.html',
-  './logo.png',
-  './harga.json',
-  './manifest.json'
-];
-
-// Install Service Worker
+const CACHE_NAME = 'yeny-gorden-v1';
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html', './manifest.json'])));
 });
 
-// Strategi: Network First, Fallback to Cache
-// Bagus untuk harga.json agar selalu update jika ada sinyal, tapi pakai cache jika offline
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
